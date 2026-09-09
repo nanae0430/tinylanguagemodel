3187c724948f76ad925f8a13091a1c56d8c750ed
nanae0430 <515403546@qq.com>
2026-09-09 15:12:40 +0800

    update

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from MinBPE import RegexTokenizer
-from tinylanguagemodel import TinyLanguageModel, train, estimate_loss
+from tinylanguagemodel import TinyLanguageModel, train, estimate_loss, get_batch
 import time, torch
 
 torch.manual_seed(39)
-vocab_size = 1000
+
+with open("./train_text.txt", "r", encoding="utf-8") as f:
+    train_text = f.read()
+with open("./val_text.txt", "r", encoding="utf-8") as f:
+    val_text = f.read()
+
+
 batch_size = 16
 block_size = 128
 n_embd = 128
@@ -13,75 +19,28 @@ steps = 10000
 eval_iters = 100
 lr = 0.001
 device = "cuda" if torch.cuda.is_available() else "cpu"
-# with open("./tiny_shakespeare.txt", "r", encoding="utf-8") as f:
-#     corpus = f.read()
-# start_time = time.time()
-# tokenizer = RegexTokenizer()
-# tokenizer.train(
-#     text=corpus,
-#     vocab_size=1000,
-# )
-# tokenizer.save("tiny_1")
-# end_time = time.time()
-# print((end_time - start_time), "s")
-# tokenizer = RegexTokenizer()
-# tokenizer.load("./tiny_1.model")
-# ids = tokenizer.encode(text=corpus)
-# ids_trained = torch.tensor(
-#     data=ids,
-#     dtype=torch.long,
-#     device="cpu",
-# )
-# torch.save(ids_tensor, "tiny_token.pt")
-# ids = torch.load("./tiny_token.pt")
-# ids = ids.to("cpu")
-# train_ids, val_ids = ids[: int(len(ids) * 0.9)], ids[int(len(ids) * 0.9) :]
-# model = TinyLanguageModel(
-#     vocab_size=vocab_size,
-#     n_embd=n_embd,
-#     block_size=block_size,
-#     num_head=num_head,
-#     num_layer=num_layer,
-#     dropout=0.1,
-# )
-# model.to(device)
-# optimizer = torch.optim.AdamW(params=model.parameters(), lr=lr)
-# train(
-#     model=model,
-#     optimizer=optimizer,
-#     train_data=train_ids,
-#     val_data=val_ids,
-#     batch_size=batch_size,
-#     block_size=block_size,
-#     steps=steps,
-#     eval_iters=eval_iters,
-#     device=device,
-# )
-model = TinyLanguageModel(
-    vocab_size=vocab_size,
-    n_embd=n_embd,
-    block_size=block_size,
-    num_head=num_head,
-    num_layer=num_layer,
-)
-best_checkpoint = torch.load("./best_checkpoint.pt")
-model.load_state_dict(best_checkpoint["model_state_dict"])
-model.to(device)
-model.eval()
 
 tokenizer = RegexTokenizer()
-tokenizer.load("./tiny_1.model")
-idx = tokenizer.encode("I'm Miku.")
-idx = torch.tensor(data=idx, dtype=torch.long, device=device).unsqueeze(0)
-# 贪心生成：多次运行结果相同
-output = model.generate(idx, 100, top_k=1)[0].tolist()
-print(tokenizer.decode(output))
-print("-" * 20)
-# Top-K采样
-output = model.generate(idx, 100, top_k=20, temperature=0.8)[0].tolist()
-print(tokenizer.decode(output))
-print("-" * 20)
-# 全词表采样
-output = model.generate(idx, 100, top_k=None, temperature=0.8)[0].tolist()
-print(tokenizer.decode(output))
-print("-" * 20)
+tokenizer.load("./tiny_story.model")
+vocab_size = len(tokenizer.vocab)
+
+train_data = tokenizer.encode(train_text, {"<|endoftext|>": 2000})
+val_data = tokenizer.encode(val_text, tokenizer.special_tokens)
+train_data = torch.tensor(train_data, dtype=torch.long)
+val_data = torch.tensor(val_data, dtype=torch.long)
+
+model = TinyLanguageModel(
+    vocab_size, n_embd, block_size, num_head, num_layer, dropout=0.1
+)
+optimizer = torch.optim.AdamW(params=model.parameters(), lr=5e-4)
+train(
+    model=model,
+    optimizer=optimizer,
+    train_data=train_data,
+    val_data=val_data,
+    batch_size=batch_size,
+    block_size=block_size,
+    steps=steps,
+    eval_iters=eval_iters,
+    device=device,
+)
--- tiny_story_bpe.py
+++ tiny_story_bpe.py
@@ -4,8 +4,11 @@ import time
 tokenizer = RegexTokenizer()
 with open("./TinyStories-valid.txt", "r", encoding="utf-8") as f:
     text = f.read()
-train_text = text[: int(0.9 * len(text))]
-test_text = text[int(0.9 * len(text)) :]
+eos_token = "<|endoftext|>"
+raw_split_index = int(0.9 * len(text))
+eos_index = text.rfind(eos_token, 0, raw_split_index)
+train_text = text[0 : eos_index + len(eos_token)]
+val_text = text[eos_index + len(eos_token) :]
 
 start_time = time.time()
 special_tokens = {"<|endoftext|>": 2000}
@@ -14,3 +17,7 @@ tokenizer.train(corpus=train_text, vocab_size=2000)
 tokenizer.save("tiny_story")
 end_time = time.time()
 print(f"{(end_time - start_time):.3f}s")
+with open("./train_text.txt", "w", encoding="utf-8") as f:
+    f.write(train_text)
+with open("./val_text.txt", "w", encoding="utf-8") as f:
+    f.write(val_text)
